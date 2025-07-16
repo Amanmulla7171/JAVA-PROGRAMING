8c9e9689e4bcd11dd625e2315465ab797e42f5f4
Aman Mulla <163362618+Amanmulla7171@users.noreply.github.com>
2025-07-16 22:22:57 +0530

    Add files via upload

---
+++ Datetest.java
@@ -0,0 +1,19 @@
+package com.time;
+
+public class Datetest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = new Date();
+		
+	    date.accept();
+		date.display();
+		
+		//Date dt2=null;
+		//dt2.accept();
+		
+		//dt2.display();
+		
+	}
+
+}
